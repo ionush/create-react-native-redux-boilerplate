25f37643db8c1010cba527c3a5eb0777dd1f029d
ionush <neilasjoseph@gmail.com>
2018-02-10 14:47:56 +0200

    made corrections to eslint and added missing thunk package

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: ['react', 'react-native'],
   rules: {
     'react/jsx-uses-vars': 2,
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
   },
   parser: 'babel-eslint',
 };
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,14 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import promiseMiddleware from "redux-promise-middleware";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-	rootReducer,
-	{},
-	composeEnhancers(applyMiddleware(promiseMiddleware()))
+  rootReducer,
+  {},
+  composeEnhancers(applyMiddleware(thunk, promiseMiddleware())),
 );
 
 export default store;
